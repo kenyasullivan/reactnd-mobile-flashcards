0dca961a32907bd0f094250cf6558081735e9a2d
James Bunt <james.bunt@vendhq.com>
2017-10-27 19:31:04 +1300

    Scaffold initial tab layout

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Platform, View, StatusBar } from 'react-native'
+import { Constants } from 'expo'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import DeckList from './components/DeckList'
+import CreateDeck from './components/CreateDeck'
+import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons'
+import { red, white } from './utils/colors'
+
+const Tabs = TabNavigator(
+  {
+    DeckList: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: 'Decks',
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialCommunityIcons
+            name='cards-outline'
+            size={30}
+            color={tintColor}
+          />
+        )
+      }
+    },
+    CreateDeck: {
+      screen: CreateDeck,
+      navigationOptions: {
+        tabBarLabel: 'New Deck',
+        tabBarIcon: ({ tintColor }) => (
+          <Entypo name='new-message' size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? red : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ? white : red,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  }
+)
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs
+  }
+})
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{ backgroundColor: red, height: Constants.statusBarHeight }}
+        >
+          <StatusBar
+            translucent
+            backgroundColor={red}
+            barStyle='light-content'
+          />
+        </View>
+        <MainNavigator />
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
